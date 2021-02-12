b64f3f9066ed0d24b25c0a805136564303016e2a
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-02-12 21:19:09 +0530

    Extracting Hook Logic

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,35 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResuts from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async (searchTerm) => {
-    console.log("Hi there!");
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage("Something went wrong");
-    }
-  };
-
-  // Call searchApi when component
-  // is first rendered
-  // searchApi("pasta");
-  useEffect(() => {
-    searchApi("pasta");
-  }, []);
+  const [searchApi, results, errorMessage] = useResuts();
 
   return (
     <View>
@@ -40,6 +17,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList />
+      <ResultsList />
+      <ResultsList />
     </View>
   );
 };
